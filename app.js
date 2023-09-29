df611043812b2e620cd43864c8e8eb191cf2ac43
Membership12 <146455819+Membership12@users.noreply.github.com>
2023-09-29 15:30:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_w7gv1z8", "template_f2o87qe", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
